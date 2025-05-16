e73153132eac57d76ec7701b25116e9c49bd5f71
Paul-1511 <paulmendez1511@gmail.com>
2025-05-15 21:33:17 -0600

    feat: preview built

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -1,7 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // 👈 Esto es lo importante
   plugins: [react()],
 })
